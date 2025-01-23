4e2e19ca150e436daf38995afcc1b129722dfb0c
SharkMan <jhoel821@gmail.com>
2025-01-23 17:45:06 -0400

    finish pizza conditional pricing exercise

---
+++ Day 3/Modulo/task.py
@@ -0,0 +1,5 @@
+number = int(input("write a number to check if is even or odd: \n"))
+if number % 2 == 0:
+    print("even")
+else:
+    print("odd")
--- Day 3/Python Pizza/task.py
+++ Day 3/Python Pizza/task.py
@@ -1,5 +1,31 @@
-print("Welcome to Python Pizza Deliveries!")
-size = input("What size pizza do you want? S, M or L: ")
-pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
-extra_cheese = input("Do you want extra cheese? Y or N: ")
+# create a dic with the prices
+PIZZA_PRICES = {"S": 15, "M": 20, "L": 25}
+PIZZA_NAME = {"S": "Cheap Ass Pizza", "M": "Not so Hungry Pizza", "L": "`Stop you fat fuck` Pizza"}
+PEPPERONI_COST = {"S": 2, "M": 3, "L": 3}
+EXTRA_CHEESE_COST = 1
 
+def calculate_bill(size, add_pepperoni, add_extra_cheese):
+    # Base price based on size
+    if size not in PIZZA_PRICES:
+        raise ValueError("Invalid pizza size selected.")
+    bill = PIZZA_PRICES[size]
+
+    # Add costs for extras
+    if add_pepperoni == "Y":
+        bill += PEPPERONI_COST[size]
+    if add_extra_cheese == "Y":
+        bill += EXTRA_CHEESE_COST
+
+    return bill
+
+
+print("Welcome to Python Pizza Deliveries!")
+try:
+    size = input("What size pizza do you want? S, M or L: ").upper()
+    add_pepperoni = input("Do you want pepperoni on your pizza? Y or N: ")
+    add_extra_cheese = input("Do you want extra cheese? Y or N: ")
+
+    total_bill = calculate_bill(size, add_pepperoni, add_extra_cheese)
+    print(f"Your {PIZZA_NAME[size]} cost is: ${total_bill}.")
+except ValueError as e:
+    print(e)
