a15209092d0f049c670a1baf8f2cb1a813a6466a
SharkMan <jhoel821@gmail.com>
2025-01-23 17:07:20 -0400

    start the first 15 days.

--- Day 2/Number Manipulation/task.py
+++ Day 2/Number Manipulation/task.py
@@ -1,1 +1,2 @@
 bmi = 84 / 1.65 ** 2
+print(f"your Body Mass Index is:  {round(bmi)}")
--- Day 2/Tip Calculator Project/task.py
+++ Day 2/Tip Calculator Project/task.py
@@ -1,6 +1,12 @@
+def calculate_payment(total_bill, tip_percentage, num_people):
+    total_with_tip = total_bill * (1 + tip_percentage)
+    return round(total_with_tip / num_people, 2)
+
+
 print("Welcome to the tip calculator!")
 bill = float(input("What was the total bill? $"))
-tip = int(input("What percentage tip would you like to give? 10 12 15 "))
+tip_percentage = int(input("What percentage tip would you like to give? 10 12 15 ")) / 100
 people = int(input("How many people to split the bill? "))
 
-
+individual_payment = calculate_payment(bill, tip_percentage, people)
+print(f"Each person should pay: ${individual_payment:.2f}")
